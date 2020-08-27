2fb50ac16a28672316058470796986b182899587
VitorEltz <hethell7@gmail.com>
2020-08-27 20:42:42 -0300

    Home Screen

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/recomends_plants.dart';
 import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
+import 'featured_plants.dart';
 import 'header_with_serachbox.dart';
 
 class Body extends StatelessWidget {
@@ -13,64 +15,10 @@ class Body extends StatelessWidget {
         children: <Widget>[
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recomended", press: () {}),
-          Container(
-            margin: EdgeInsets.only(
-              left: kDefaultPadding,
-              top: kDefaultPadding / 2,
-              bottom: kDefaultPadding * 2.5,
-            ),
-            width: size.width * 0.4,
-            child: Column(
-              children: <Widget>[
-                Image.asset("assets/images/image_1.png"),
-                Container(
-                  padding: EdgeInsets.all(kDefaultPadding / 2),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23)
-                      ),
-                    ]
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "Samantha\n".toUpperCase(),
-                              style: Theme.of(context).textTheme.button,
-                            ),
-                            TextSpan(
-                              text: "Russia".toUpperCase(),
-                              style: TextStyle(
-                                color: kPrimaryColor.withOpacity(0.5),
-                              )
-                            )
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        '\$440', 
-                        style: Theme.of(context)
-                        .textTheme
-                        .button
-                        .copyWith(color: kPrimaryColor),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
         ],
       ),
     );
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:plant_app/components/my_bottom_nav_bar.dart';
+import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,6 +10,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 
