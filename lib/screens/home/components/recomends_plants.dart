624779b1697d1b52fda4586c3fe4ee218427a81a
Vitor Eltz <vitor.eltz@azion.com>
2020-08-29 17:57:46 -0300

    Details Screen

--- lib/screens/home/components/recomends_plants.dart
+++ lib/screens/home/components/recomends_plants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/details/details_screen.dart';
 
 import '../../../constants.dart';
 
@@ -14,26 +15,38 @@ class RecomendsPlants extends StatelessWidget {
       child: Row(
         children: <Widget>[
           RecomendPlantCard(
-            image: "assets/images/image_1.png",
-            title: "Samantha",
-            country: "Russia",
-            price: 440,
-            press: () {}
-          ),
+              image: "assets/images/image_1.png",
+              title: "Samantha",
+              country: "Russia",
+              price: 440,
+              press: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailsScreen(),
+                  ),
+                );
+              }),
           RecomendPlantCard(
-            image: "assets/images/image_2.png",
-            title: "Angelica",
-            country: "Russia",
-            price: 440,
-            press: () {}
-          ),
+              image: "assets/images/image_2.png",
+              title: "Angelica",
+              country: "Russia",
+              price: 440,
+              press: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailsScreen(),
+                  ),
+                );
+              }),
           RecomendPlantCard(
-            image: "assets/images/image_3.png",
-            title: "Solange",
-            country: "Russia",
-            price: 440,
-            press: () {}
-          ),
+              image: "assets/images/image_3.png",
+              title: "Solange",
+              country: "Russia",
+              price: 440,
+              press: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailsScreen(),
+                  ),
+                );
+              }),
         ],
       ),
     );
